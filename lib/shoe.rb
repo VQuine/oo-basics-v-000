7fd91717508cf0340168976587ea0d9fbc4b5dbe
Kimberly Aviles <01010110.v@gmail.com>
2018-11-18 01:07:42 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,4 +2,22 @@ class Shoe
   def initialize(brand)
     @brand = brand
   end
+  def brand
+    @brand
+  end
+
+  def color=(color)
+    @color
+  end
+  def color
+    @color
+  end
+
+    def size=(size)
+      @size = size
+    end
+    def size
+      @size
+    end
+
 end
