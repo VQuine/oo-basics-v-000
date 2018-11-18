031cc932042b3d016e6675eb09d755acc581d03c
Kimberly Aviles <01010110.v@gmail.com>
2018-11-18 00:55:15 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -3,6 +3,35 @@
 class Book
 
   def initialize(title)
+    @title = title
   end
+
+  def title
+  end
+
+  def author=(author)
+    @author = author
+  end
+
+  def author
+    @author = author
+  end
+
+  def page_count=(num)
+    @page_count = num
+  end
+
+  def page_count
+    @page_count
+  end
+
+  def genre=(genre)
+    @genre = genre
+  end
+
+  def genre
+    @genre = genre
+  end
+    
 
 end
